99eb94b3cbcac8932a0d21c5208649f5f47519ff
hansenjacobs <hansenjacobs@gmail.com>
2022-11-04 17:06:56 -0500

    completed user registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,3 +9,11 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ['first_name', 'last_name', 'username',
                   'email', 'phone_number', 'password']
+
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data['password']
+        confirm_password = cleaned_data['confirm_password']
+
+        if password != confirm_password:
+            raise forms.ValidationError("Password does not match.")
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import UserForm
 from .models import User
+from django.contrib import messages
 
 # Create your views here.
 
@@ -10,10 +11,33 @@ def registerUser(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
+            # Create User directly w/ Object
+            # password = form.cleaned_data['password']
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # user.save()
+            # return redirect('registerUser')
+
+            # Create User using Object Method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            phone = form.cleaned_data['phone_number']
+
+            user = User.objects.create_user(
+                first_name, last_name, username, email, password)
+            user.phone_number = phone
             user.role = User.CUSTOMER
             user.save()
+
+            messages.success(request, 'Your account has been created.')
+
             return redirect('registerUser')
+        else:
+            pass
     else:
         form = UserForm()
 
